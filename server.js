7d9ba933f68e6a532e7bf0e39ff832007304e8e3
Salendra Singh Yadav <singhsalendra095@gmail.com>
2026-08-20 14:03:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,47 @@
 const express = require('express');
 const path = require('path');
-const  ejs = require('ejs');
+const ejs = require('ejs');
 const cookieParser = require('cookie-parser');
 
 const adminRoutes = require('./routes/adminRoutes');
-const dataSeeding = require('./routes/dataSeeding')
+const dataSeeding = require('./routes/dataSeeding');
 const studentRoutes = require('./routes/studentRoutes');
 const professorRoutes = require('./routes/professorRoutes');
 const hodRoutes = require('./routes/hodRoutes');
 const authRoutes = require('./routes/authRoutes');
-const DBconnect = require("./model/schema/DbConnect");
+const DBconnect = require('./model/schema/DbConnect');
 
 DBconnect();
+
 // dataSeeding.departmentSeeding()
-// dataSeeding.Userseeding()    
+// dataSeeding.Userseeding()
 // dataSeeding.assignmentSeeding()
 
-
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.set('view engine', 'ejs');
+
 app.set('views', [
   path.join(__dirname, 'views/admin'),
   path.join(__dirname, 'views/student'),
-   path.join(__dirname, 'views/professor'),
+  path.join(__dirname, 'views/professor'),
 ]);
 
-
 app.get('/', (req, res) => {
-  res.render("login",{email:"",msg1:"",msg2:""});
+  res.render("login", {
+    email: "",
+    msg1: "",
+    msg2: ""
+  });
 });
-
-
 
 app.use('/', adminRoutes);
 app.use('/', studentRoutes);
@@ -45,10 +49,10 @@ app.use('/', professorRoutes);
 app.use('/', hodRoutes);
 app.use('/', authRoutes);
 
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
 
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-
+module.exports = app;
