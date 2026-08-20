c6018c24326d7e2f38fc1d988cb9c0840a870202
Salendra Singh Yadav <singhsalendra095@gmail.com>
2026-08-20 14:05:29 +0530

    Use environment variable for MongoDB connection URL

--- model/schema/DbConnect.js
+++ model/schema/DbConnect.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 
 const mongoDBUrl = process.env.MONGODB_URL;
 
-function DBconnect(){
-mongoose.connect("mongodb+srv://singhsalendra095_db_user:<db_password>@cluster0.ar46wut.mongodb.net/Assigment")
-.then(()=> console.log(" Database connected ...."))
-.catch((err)=> console.log("error occured   :  " + err));
+function DBconnect() {
+  mongoose.connect(mongoDBUrl)
+    .then(() => console.log("Database connected ...."))
+    .catch((err) => console.log("error occured : " + err));
 }
-
 
 module.exports = DBconnect;
